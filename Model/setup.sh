003df1455394acd0609eb94d5578d85d6f69c720
Karthik Suresh <karthiksuresh324@gmail.com>
2023-12-12 03:52:14 -0600

    Notebooks updated, xlcost new files added, xlcost seed asserts added

--- Model/setup.sh
+++ Model/setup.sh
@@ -1,8 +1,7 @@
-apt-get update
-apt-get install sudo
-sudo apt-get install git-lfs
-sudo apt-get install unzip
+# for openai
+# sudo apt install python3.12 
 pip install peft
+pip install openai
 pip install bitsandbytes loralib --upgrade
 pip install uuid
 pip install accelerate
@@ -11,10 +10,15 @@ pip3 install -U click
 pip3 install scipy
 pip3 install datasets
 pip install sentence-transformers
+pip install matplotlib
 pip install trl
 pip install -U git+https://github.com/huggingface/transformers.git 
 pip install -U git+https://github.com/huggingface/peft.git
 pip install -U git+https://github.com/huggingface/accelerate.git
+apt-get update
+apt-get install sudo
+sudo apt-get install git-lfs
+sudo apt-get install unzip
 
 # git clone https://huggingface.co/meta-llama/Llama-2-13b-hf
 # cd Llama-2-13b-hf
@@ -38,15 +42,15 @@ pip install -U git+https://github.com/huggingface/accelerate.git
 # git-lfs install
 # git lfs pull
 
-git clone https://huggingface.co/intfloat/e5-base-v2
-cd e5-base-v2
-git-lfs install
-git lfs pull
+# git clone https://huggingface.co/intfloat/e5-base-v2
+# cd e5-base-v2
+# git-lfs install
+# git lfs pull
 
-git clone https://huggingface.co/intfloat/e5-large-v2
-cd e5-large-v2
-git-lfs install
-git lfs pull
+# git clone https://huggingface.co/intfloat/e5-large-v2
+# cd e5-large-v2
+# git-lfs install
+# git lfs pull
 
 
 # git config --global user.email "karthiksuresh324@gmail.com"
